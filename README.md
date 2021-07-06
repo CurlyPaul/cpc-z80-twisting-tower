cb4bad416996e635aa76274e4d85a4c81ba046ce
Paul Horton <paul.horton@purplebricks.com>
2021-07-06 01:41:19 +0100

    Added black lines

---
+++ README.md
@@ -0,0 +1,34 @@
+# cpc-z80-tower-poc
+
+## Introduction
+
+A simple POC for a tower effect that could be used in a demo. 
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=9Qd0ZVBiQME
+" target="_blank"><img src="http://img.youtube.com/vi/9Qd0ZVBiQME/0.jpg" 
+alt="Version 0.1" width="240" height="180" border="10" /></a>
+
+Written in Z80 Assembler for the Amstrad CPC.
+
+Built using the tutorials and examples found at https://www.chibiakumas.com/z80/index.php
+
+Inspired by the awesome [Still Rising by Vanity](https://www.youtube.com/watch?v=wtuojO0Zc90)
+
+Also inspired by the excellent [Coding Secrets Mickey Mania](https://www.youtube.com/watch?v=fV9BqqU17DM) video
+
+
+## Building and Running
+
+### Prerequisites 
+
+Download and extract WinApe from http://www.winape.net/
+
+WinApe is a CPC emulator with a build in Assembler.
+
+Load WinApe. Press F3 to open the editor, and from there load main.asm.
+
+Press F9 to assemble the file
+
+In the emulator type `call &8000`
+
+
--- main.asm
+++ main.asm
@@ -2,22 +2,8 @@
 ; Header
 ;****************************************
 ScreenSize equ &4000
-
-RowOffset_XPos equ 0
-RowOffset_YPos equ 1
-RowOffset_Height equ 2
-RowOffset_BlockWidth equ 3
-RowOffset_LHWidth equ 4
-RowOffset_RHWidth equ 5
-RowOffset_Hue equ 6
-
-Pallete_Black equ 14
-		  ;; pixel A 	B
-HeatMap_0 equ %00000000 ;; 0	0
-HeatMap_1 equ %11000000 ;; 1	1
-HeatMap_2 equ %10000100	;; 1    2 ??? Toggle this 
-HeatMap_3 equ %00001100 ;; 2    2
-HeatMap_4 equ %11001100 ;; 3    3
+Palette_Background equ &FF
+Palette_Black equ &3F
 
 org &8000
 
@@ -26,68 +12,21 @@ call Palette_Init
 call InterruptHandler_Init
 call MainLoop
 
-HeatMap:
-db HeatMap_0
-db %10000000
-db HeatMap_1
-db HeatMap_1
-db HeatMap_1
-db HeatMap_2
-db HeatMap_2 
-db HeatMap_2 
-db %10011000 
-db HeatMap_3 
-db HeatMap_3
-db HeatMap_3
-db HeatMap_4
-
-db HeatMap_4
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-db HeatMap_3
-
-db HeatMap_4
-db HeatMap_3  
-db HeatMap_3  
-db HeatMap_3  
-db HeatMap_3 
-db HeatMap_2  
-db HeatMap_2  
-db HeatMap_2  
-db HeatMap_1  
-db HeatMap_1  
-db HeatMap_1  
-db HeatMap_1 
-db HeatMap_0
-db HeatMap_0
-
-
 ;****************************************
 ; Main Program
 ;****************************************
 MainLoop:
 	call DrawBackground
 
-	ld iy,Row1Template
+	ld iy,Row1Struct
 	call DrawRow
-	ld iy,Row2Template
+	ld iy,Row2Struct
 	call DrawRow
-	ld iy,Row3Template
+	ld iy,Row3Struct
 	call DrawRow
-	ld iy,Row4Template
+	ld iy,Row4Struct
 	call DrawRow
-	ld iy,Row5Template
+	ld iy,Row5Struct
 	call DrawRow
 	
 	ld ix,FrameSemaphor
@@ -109,7 +48,7 @@ ClearScreen:
 		ld de,(BackBufferAddress)
 		inc de
 		ld bc,ScreenSize-1
-		ld (hl),&FF
+		ld (hl),Palette_Background
 		ldir
 	pop de
 ret
@@ -117,6 +56,8 @@ ret
 DrawRow:
 	;; INPUTS
 	;; IY Row struct
+	call DrawBlock
+	
 	ld b,(iy+RowOffset_XPos)
 	ld c,(iy+RowOffset_YPos)
 	ld d,(iy+RowOffset_Height)
@@ -151,8 +92,7 @@ SkipLeftReset:
 	ld b,a  			;; put the final X back into b
 	ld e,(iy+RowOffset_BlockWidth)	;; this one is always the standard width
 	call DrawSquare
-	
-	
+
 	;; Another square the same as the last
 	ld a,b
 	add e
@@ -252,6 +192,44 @@ DrawSquare:
 	xor 3
 	ld l,a
 	ld (ColourMaskOffsetPlus2-2),hl
+ret
+
+DrawBlock:
+	;; INPUTS 
+	;; IY Row struct
+	;; De
+	ld b,(iy+RowOffset_XPos)
+	ld c,(iy+RowOffset_YPos)
+
+	call GetScreenPos 	;; HL = screen position
+	;; init the loop counters
+	
+	;; for the width we use 3(block width+1)
+	ld e,(iy+RowOffset_BlockWidth)	
+	inc e 	;; here's the +1
+	ld a,e 	;; then add it to itself 3 times
+	add e
+	add e
+	ld c,a  ;; C == Width in Bytes
+	
+	ld a,(iy+RowOffset_Height) ; Height in lines
+	add 3  	;; to make them the same as the vertical ones	
+	ld b,a 	;; B == Height in lines
+	
+	BlockNextLine:
+		push hl
+		push bc
+
+		BlockNextByte:
+			ld a,Palette_Black
+			ld (hl),a	;; HL = Screen desintation
+			inc hl
+			dec c
+			jr nz,BlockNextByte
+		pop bc
+		pop hl
+		call GetNextLine 		
+		djnz BlockNextLine	;; djnz - decreases b and jumps when it's not zero
 ret
 
 GetScreenPos:
@@ -369,62 +347,117 @@ BackBufferAddress: 	dw &4000
 FrameCounter: 		db 0
 FrameSemaphor:		db 0
 
-;; A0	B0	A2	B2	A1	B1	A3	B3
-;; 00000000 - 0 0
-;; 01000000 - 0 1
-;; 11000000 - 1 1
-;; 10000100 - 1 2
-;; 00001100 - 2 2
-;; 01001100 - 2 3
-;; 11001100 - 3 3
-;; 10011000 - 3 4
-;; 00110000 - 4 4
-;; 01110000 - 4 5
-;; 11110000 - 5 5	
-Row1Template:
+RowOffset_XPos equ 0
+RowOffset_YPos equ 1
+RowOffset_Height equ 2
+RowOffset_BlockWidth equ 3
+RowOffset_LHWidth equ 4
+RowOffset_RHWidth equ 5
+RowOffset_Hue equ 6
+
+Row1Struct:
 	db &0F 		;; X pos
 	db &00 		;; Y pos
 	db &30 		;; Height
-	db &0D 		;; Width
+	db &0D 		;; Block Width
 	db &0D 		;; Current LH square width
 	db &0  		;; Current RH square width
 	db %00000000 	;; Starting hue
 
-Row2Template:
+Row2Struct:
 	db &0F 		;; X pos
 	db &33 		;; Y pos
 	db &30 		;; Height
-	db &0D 		;; Width
+	db &0D 		;; Block Width
 	db &06 		;; Current LH square width
 	db &07 		;; Current RH square width
 	db %00000011 	;; Starting hue
 
-Row3Template:
+Row3Struct:
 	db &0F 		;; X pos
 	db &66 		;; Y pos
 	db &10 		;; Height
-	db &0D 		;; Width
+	db &0D 		;; Block Width
 	db &03 		;; Current LH square width
 	db &0A 		;; Current RH square width
 	db %00000000 	;; Starting hue
 
-Row4Template:
+Row4Struct:
 	db &0F 		;; X pos
 	db &79 		;; Y pos
 	db &30 		;; Height
-	db &0D 		;; Width
+	db &0D 		;; Block Width
 	db &0B 		;; Current LH square width
 	db &02 		;; Current RH square width
 	db %00000011 	;; Starting hue
 
-Row5Template:
+Row5Struct:
 	db &0F 		;; X pos
 	db &AC 		;; Y pos
 	db &18 		;; Height
-	db &0D 		;; Width
+	db &0D 		;; Block Width
 	db &09 		;; Current LH square width
 	db &04  	;; Current RH square width
 	db %00000000 	;; Starting hue
 
 read ".\libs\CPC_V1_SimpleScreenSetUp.asm"
 read ".\libs\CPC_V1_SimplePalette.asm"
+
+;****************************************
+; Resources
+;****************************************
+
+;; Pixel layout: A0 B0 A2 B2 A1 B1 A3 B3
+
+		  ;; pixel A 	B
+HeatMap_0 equ %00000000 ;; 0	0
+HeatMap_1 equ %11000000 ;; 1	1
+HeatMap_2 equ %10000100	;; 1    2  
+HeatMap_3 equ %00001100 ;; 2    2
+HeatMap_4 equ %11001100 ;; 3    3
+
+HeatMap:
+	db HeatMap_0
+	db %10000000
+	db HeatMap_1
+	db HeatMap_1
+	db HeatMap_1
+	db HeatMap_2
+	db HeatMap_2 
+	db HeatMap_2 
+	db %10011000 
+	db HeatMap_3 
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_4
+
+	db HeatMap_4
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+	db HeatMap_3
+
+	db HeatMap_4
+	db HeatMap_3  
+	db HeatMap_3  
+	db HeatMap_3  
+	db HeatMap_3 
+	db HeatMap_2  
+	db HeatMap_2  
+	db HeatMap_2  
+	db HeatMap_1  
+	db HeatMap_1  
+	db HeatMap_1  
+	db HeatMap_1 
+	db HeatMap_0
+	db HeatMap_0
